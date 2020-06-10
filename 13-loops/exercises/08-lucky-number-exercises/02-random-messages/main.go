42bbaff70ea0ec2065e9d5bf80c49f09d7e4dead
Zhou Wei <lilaboc.cn@gmail.com>
2020-06-10 11:04:21 +0800

    lucky

--- 13-loops/exercises/08-lucky-number-exercises/02-random-messages/main.go
+++ 13-loops/exercises/08-lucky-number-exercises/02-random-messages/main.go
@@ -8,6 +8,14 @@
 
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: Random Messages
 //
@@ -34,5 +42,72 @@ package main
 //    YOU LOST. TRY AGAIN?
 // ---------------------------------------------------------
 
+const (
+	maxTurns = 5 // less is more difficult
+	usage    = `Welcome to the Lucky Number Game! 🍀
+
+The program will pick %d random numbers.
+Your mission is to guess one of those numbers.
+
+The greater your number is, harder it gets.
+
+Wanna play?
+`
+)
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	args := os.Args[1:]
+
+	if len(args) != 1 {
+		fmt.Printf(usage, maxTurns)
+		return
+	}
+
+	guess, err := strconv.Atoi(args[0])
+	if err != nil {
+		fmt.Println("Not a number.")
+		return
+	}
+
+	if guess < 0 {
+		fmt.Println("Please pick a positive number.")
+		return
+	}
+
+	for turn := 1; turn <= maxTurns; turn++ {
+		n := rand.Intn(guess + 1)
+
+		// Better, why?
+		//
+		// Instead of nesting the if statement into
+		//   another if statement; it simply continues.
+		//
+		// TLDR: Avoid nested statements.
+		if n != guess {
+			continue
+		}
+
+		if turn == 1 {
+			fmt.Println("🥇 FIRST TIME WINNER!!!")
+		} else {
+			switch rand.Intn(2) {
+			case 0:
+				fmt.Println("YOU'RE AWESOME")
+			case 1:
+				fmt.Println("🎉  YOU WON!")
+			}
+
+		}
+		return
+	}
+
+	switch rand.Intn(2) {
+	case 0:
+		fmt.Println("☠️  YOU LOST... Try again?")
+	case 1:
+		fmt.Println("Loser!")
+
+	}
 }
