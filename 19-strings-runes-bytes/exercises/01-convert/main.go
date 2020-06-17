171932194f7ef7d0099ddf34ed4fc0cabd6d9902
stern <lilaboc.cn@gmail.com>
2020-06-17 14:36:30 +0800

    string

--- 19-strings-runes-bytes/exercises/01-convert/main.go
+++ 19-strings-runes-bytes/exercises/01-convert/main.go
@@ -8,6 +8,8 @@
 
 package main
 
+import "fmt"
+
 // ---------------------------------------------------------
 // EXERCISE: Convert the strings
 //
@@ -34,12 +36,21 @@ package main
 func main() {
 	// Please uncomment the code below
 
-	// words := []string{
-	// 	"gopher",
-	// 	"programmer",
-	// 	"go language",
-	// 	"go standard library",
-	// }
+	words := []string{
+		"gopher",
+		"programmer",
+		"go language",
+		"go standard library",
+	}
 
-	// var bwords [][]byte
+	var bwords [][]byte
+	for _, word := range words {
+		bwords = append(bwords, []byte(word))
+	}
+
+	for _, word := range bwords {
+		fmt.Println(word)
+		fmt.Println(string(word))
+	}
+
 }
--- 19-strings-runes-bytes/exercises/02-print-the-runes/main.go
+++ 19-strings-runes-bytes/exercises/02-print-the-runes/main.go
@@ -8,6 +8,8 @@
 
 package main
 
+import "fmt"
+
 // ---------------------------------------------------------
 // EXERCISE: Print the runes
 //
@@ -33,4 +35,19 @@ package main
 
 func main() {
 	const word = "console"
+	for _, w := range word {
+		fmt.Printf("%c\n", w)
+		fmt.Printf("\tdecimal: %[1]d\n", w)
+		fmt.Printf("\thex    : 0x%[1]x\n", w)
+		fmt.Printf("\tbinary : 0b%08[1]b\n", w)
+	}
+
+	fmt.Printf("with runes       : %s\n",
+		string([]byte{'c', 'o', 'n', 's', 'o', 'l', 'e'}))
+
+	fmt.Printf("with decimals    : %s\n",
+		string([]byte{99, 111, 110, 115, 111, 108, 101}))
+
+	fmt.Printf("with hexadecimals: %s\n",
+		string([]byte{0x63, 0x6f, 0x6e, 0x73, 0x6f, 0x6c, 0x65}))
 }
